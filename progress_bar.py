3b921f3c00cab02198dc12d783e51f784e59b471
zhukerui <zhukerui@kanzhun.com>
2018-10-20 17:31:12 +0800

    add progress percentage

--- progress_bar.py
+++ progress_bar.py
@@ -10,7 +10,7 @@ def progress_bar(itera, func, *args, **kwargs):
     n = float(len(itera))
     cnt = 0
     for i in itera:
-        sys.stdout.write('progress: | {0}{1} | \r'.format('>'*int((cnt/n)*50), '='*(50-int((cnt/n)*50))))
+        sys.stdout.write('progress: | {0}{1} |  {2}\% \r'.format('>'*int((cnt/n)*50), '='*(50-int((cnt/n)*50))), str(cnt/n)*100))
         sys.stdout.flush()  # 强制程序执行IO, 否则它将累计一定批量或者时间才执行
         func(i, *args, **kwargs)
         cnt += 1
