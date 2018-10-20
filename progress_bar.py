a8b0647057939ccfade71ad507cea9f5e5a9c5e9
zhukerui <zhukerui@kanzhun.com>
2018-10-20 17:43:25 +0800

    bug-fixed

--- progress_bar.py
+++ progress_bar.py
@@ -1,7 +1,6 @@
 # -*- coding:UTF-8 -*-
 """
 A visuable progress-bar tool for python software
-Both python2.x and python3.x are compatible
 INPUT: iterable, function, *args, **kwargs
 """
 import sys
@@ -10,9 +9,9 @@ def progress_bar(itera, func, *args, **kwargs):
     n = float(len(itera))
     cnt = 0
     for i in itera:
-        sys.stdout.write('progress: | {0}{1} |  {2}\% \r'.format('>'*int((cnt/n)*50), '='*(50-int((cnt/n)*50))), str(cnt/n)*100))
+        sys.stdout.write('progress: | {0}{1} |  {2}% \r'.format('>'*int((cnt/n)*50), '='*(50-int((cnt/n)*50)), str(round((cnt/n)*100, 1))))
         sys.stdout.flush()  # 强制程序执行IO, 否则它将累计一定批量或者时间才执行
         func(i, *args, **kwargs)
         cnt += 1
 
-    print('progress: | {0} | \r'.format('>'*50))
+    print('progress: | {0} |  100.0%'.format('>'*50))
